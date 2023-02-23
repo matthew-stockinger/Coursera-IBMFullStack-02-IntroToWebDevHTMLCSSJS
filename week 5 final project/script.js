3dd20c68544050fb8a13fb555b0d1cbea12e54cf
Matthew Stockinger <monkeygrp@gmail.com>
2023-02-22 21:17:12 -0600

    completed course final project

--- week 5 final project/script.js
+++ week 5 final project/script.js
@@ -5,14 +5,15 @@ function addRecommendation() {
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
-
+    showPopup(true);
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.setAttribute("class", "recommendation");
+    element.innerHTML =
+      "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
     // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
+    document.getElementById("all_recommendations").appendChild(element);
+
     // Reset the value of the textarea
     recommendation.value = "";
   }
@@ -20,8 +21,8 @@ function addRecommendation() {
 
 function showPopup(bool) {
   if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
+    document.getElementById("popup").style.visibility = "visible";
   } else {
-    document.getElementById('popup').style.visibility = 'hidden'
+    document.getElementById("popup").style.visibility = "hidden";
   }
 }
